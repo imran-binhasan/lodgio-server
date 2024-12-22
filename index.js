9a76356187195c4de6c7bf157f6b32076d588952
imran-binhasan <imranbinhasan.work@gmail.com>
2024-12-22 07:25:35 +0600

    Setup db and update env vars

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const {configDotenv} = require('dotenv');
+configDotenv()
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+const port = process.env.PORT || 5000;
+
+
+
+const uri = process.env.CONNECTION_STRING;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+    const database = client.db('lodgio-db');
+    const hotelList = database.collection('hotel-list');
+
+    // servers starts here ..........
+    app.get('/', async(req, res) => {
+        res.send('Server is running ...................')
+    })
+
+
+
+
+
+
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port,()=>{
+    console.log(`Listening on port : ${port}`)
+})
